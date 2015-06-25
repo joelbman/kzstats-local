d39f19cc07048f6e3e128dc1bdb1c96c0499c213
joelbman <sooletus@gmail.com>
2015-06-25 15:28:24 +0300

    Player endpoint

--- api/endpoint/player.php
+++ api/endpoint/player.php
@@ -1,3 +1,7 @@
 <?php
 
-  echo 'hello world';
+  if (!$route) {
+    $stmt = $pdo->prepare('SELECT * From playerrank LIMIT 25');
+    if ($stmt->execute)
+      echo json_encode($stmt->fetchAll(2));
+  }
--- api/index.php
+++ api/index.php
@@ -11,8 +11,17 @@
     $route = explode('/', $req);
     $endpoint = array_shift($route);
     
-    if (file_exists('endpoint/'.$endpoint.'.php'))
+    if (file_exists('endpoint/'.$endpoint.'.php')) {
+      $pdo = new PDO(
+        'mysql:host='.DB_HOST.';
+        dbname='.DB_NAME.';
+        port='.DB_PORT.';
+        charset='.DB_CHARSET, 
+        DB_USER, DB_PASS);
+
       include_once('endpoint/'.$endpoint.'.php');
+      
+    }
     else
       http_response_code(404);
   }
