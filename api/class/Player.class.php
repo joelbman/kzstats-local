03993d0199a007d010472e59771c44704d76da4b
joelbman <sooletus@gmail.com>
2015-06-29 03:41:07 +0300

    Commenting etc.

--- api/class/Player.class.php
+++ api/class/Player.class.php
@@ -8,20 +8,12 @@
       $this->db = $db;
     }
 
-    // Converts SteamID back to STEAM_X:Y:ZZZZZZZZ format
-    private function convertId($id) {
-      $idstr = (string)$id;
-      $x = substr($idstr, 0, 1);
-      $y = substr($idstr, 1, 1);
-      $z = substr($idstr, 2);
-      $steamid = 'STEAM_'.$x.':'.$y.':'.$z;
-      return $steamid;
-    }
-
+    // Returns top 25 players based on points
     public function getList() {
       return $this->db->fetchAll('SELECT * From playerrank ORDER BY points DESC LIMIT 25');
     }
 
+    // Returns given SteamID's player data
     public function getDetail($id) {
       $steamid = $this->convertId($id);
       $player = [];
@@ -31,9 +23,20 @@
       return array_merge($player, $rank, $jumpstats);
     }
 
+    // Searches for a countrycode with given country name
     private function countryCode($country) {
       include(__DIR__.'/../utils/countrycodes.php');
       return array_search($country, $countrycodes);
+    }
+
+    // Converts SteamID back to STEAM_X:Y:ZZZZZZZZ format
+    private function convertId($id) {
+      $idstr = (string)$id;
+      $x = substr($idstr, 0, 1);
+      $y = substr($idstr, 1, 1);
+      $z = substr($idstr, 2);
+      $steamid = 'STEAM_'.$x.':'.$y.':'.$z;
+      return $steamid;
     }
 
   }
