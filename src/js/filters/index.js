4a1fc7f67d23de463929c742983224e7d413012b
joelbman <sooletus@gmail.com>
2015-06-30 09:33:29 +0300

    Player detail view changes

--- src/js/filters/index.js
+++ src/js/filters/index.js
@@ -7,3 +7,17 @@ app.filter('numOnly', function() {
     return input.replace(/\D/g, '');
   };
 });
+
+// Returns full image path, used with flag icons
+// Flag icons from http://www.famfamfam.com/lab/icons/flags/
+app.filter('imgPath', function() {
+  return function(input) {
+    if (typeof input != 'undefined') {
+      if (input === '??')
+        input = 'UNK';
+      var path = 'img/' + input.toLowerCase() + '.png';
+      return path;
+    }
+    return;
+  };
+});
