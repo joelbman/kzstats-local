cdc362e8d6139dfbf6407ed944f58f74cc94265e
joelbman <sooletus@gmail.com>
2015-07-23 08:24:41 +0300

    Maplist filter menu functional

--- src/js/controllers/map/list.js
+++ src/js/controllers/map/list.js
@@ -5,6 +5,12 @@ module.exports = function($scope, MapService) {
 
   $scope.maplist = [];
 
+  $scope.fields = [
+    {name: 'Name', value: 'mapname'},
+    {name: 'Time', value: 'runtime'}
+  ];
+  $scope.orderField = $scope.fields[0];
+
   var promise = MapService.getList();
   promise.then(function(data) {
     $scope.maplist = data;
