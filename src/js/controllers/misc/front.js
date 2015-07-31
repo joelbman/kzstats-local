9c7daa76464d4374db61914b6044940a5f327da1
joelbman <sooletus@gmail.com>
2015-07-31 03:15:43 +0300

    Reshaped latest records to look more like in the global version

--- src/js/controllers/misc/front.js
+++ src/js/controllers/misc/front.js
@@ -4,6 +4,11 @@
 module.exports = /*@ngInject*/ function($scope, PlayerService) {
 
   $scope.records = [];
+  $scope.startPoint = 0;
+  $scope.pageSize = 10;
+
+  $scope.showNext = function() { $scope.startPoint += $scope.pageSize; };
+  $scope.showPrev = function() { $scope.startPoint -= $scope.pageSize; };
 
   var promise = PlayerService.getLatest();
   promise.then(function(data) { $scope.records = data; });
